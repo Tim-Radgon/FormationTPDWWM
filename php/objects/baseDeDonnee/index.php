be485998b78af04cf144db07b5d8da4e55be4ad6
Tim-Radgon <dimitri.fauve@gmail.com>
2021-12-06 14:45:47 +0100

    Exemple connection base de données

--- php/objects/baseDeDonnee/index.php
+++ php/objects/baseDeDonnee/index.php
@@ -1,10 +1,23 @@
 <?php
+require('config.php');
 
-//localhost peut être remplacé par 127.0.0.1, c'est la même chose
-//localhost est le nom de domaine
-
-$connection = new PDO('mysql:host=localhost;dbname=forum;charset=utf8', 'root', '');
+// localhost ou 127.0.0.1 c'est pareil
+// localhost c'est le nom de domaine
+try {
+    $connection = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8',
+        DB_USERNAME, DB_PASSWORD,
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+} catch (PDOException $e) {
+    if ('dev' === APP_ENV) {
+        var_dump($e);
+        die();
+    } else {
+        die('Erreur de base de donnée');
+    }
+} catch (Exception $e) {
+    die('Erreur inconnue');
+}
 
 $prep = $connection->prepare('select * from message');
 $prep->execute();
-$prep->fetch();
+var_dump($prep->fetch());
