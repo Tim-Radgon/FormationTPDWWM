cbff3bb2923099fb22f0c936954eb72dc0be4e89
Tim-Radgon <dimitri.fauve@gmail.com>
2021-12-06 11:59:27 +0100

    index base de donnée

--- php/objects/baseDeDonnee/index.php
+++ php/objects/baseDeDonnee/index.php
@@ -1,5 +1,10 @@
 <?php
 
+//localhost peut être remplacé par 127.0.0.1, c'est la même chose
+//localhost est le nom de domaine
 
 $connection = new PDO('mysql:host=localhost;dbname=forum;charset=utf8', 'root', '');
 
+$prep = $connection->prepare('select * from message');
+$prep->execute();
+$prep->fetch();
