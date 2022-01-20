5c11eb37a74d19571d26e7268356be78a500da49
Tim-Radgon <dimitri.fauve@gmail.com>
2022-01-20 08:40:22 +0100

    Amélioration forum

--- php/projet/template/header.php
+++ php/projet/template/header.php
@@ -1,35 +1,14 @@
-<header class="container-lg">
-    <div class="justify-content-center d-none d-lg-flex ">
-        <img class="logo me-1" src="apple.svg" width="40" alt="Projet Alkas"><span class="nomdusite">Alkas Projet</span>
+<header>
+    <H1>Alkas Projet</H1>
+
+
+    <div class="collapse navbar-collapse" id="navbar">
+        <ul class="navbar-nav">
+
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="des.php">jeu de des</a></li>
+            <li><a href="randNums.php">Nombre Aléatoire</a></li>
+            <li><a href="forum.php">Forum</a></li>
+        </ul>
     </div>
-
-    <nav id="main-nav" class="navbar navbar-expand-lg navbar-light ms-3 me-3">
-        <div class="navbar-brand d-sm-block d-lg-none"><img class="me-1" src="microsoft.svg" width="40"><span
-                    class="nomdusite">Alkas Projet</span></div>
-
-        <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbar"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbar">
-            <ul class="navbar-nav">
-                <li class="nav-item"><a href="../index.php" class="nav-link <?= $menu === 'accueil' ? 'active' : '' ?>">Accueil</a>
-                </li>
-                <li class="nav-item"><a href="des.php" class="nav-link <?= $menu === 'des' ? 'active' : '' ?>">jeu de
-                        des</a></li>
-
-
-                <li class="nav-item"><a href="randNums.php" class="nav-link <?= $menu === 'des' ? 'active' : '' ?>">Nombre
-                        Aléatoire</a>
-                </li>
-
-
-                <li class="nav-item"><a href="../forum.php"
-                                        class="nav-link <?= $menu === 'forum' ? 'active' : '' ?>">Forum</a></li>
-
-
-            </ul>
-        </div>
-    </nav>
 </header>
